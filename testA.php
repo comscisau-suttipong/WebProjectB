a5a27e2ca86451670458f30cf7bf82b210c5caa4
comscisau-suttipong <s5752410034@sau.ac.th>
2017-03-07 14:32:26 +0700

    Updata #1.1

---
+++ testA.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>testA</title>
+</head>
+<body>
+	<h1>TestA</h1>
+</body>
+</html>
---
+++ z.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Z</title>
+</head>
+<body>
+	<h1>Z</h1>
+</body>
+</html>
